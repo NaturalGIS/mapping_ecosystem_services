0373b0681a46debd682ffc2bfd1ec075795adf79
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2019-12-26 12:33:17 +0000

    Improved script

--- analysis_scripts/analyize_data_linux.sh
+++ analysis_scripts/analyize_data_linux.sh
@@ -1,34 +1,138 @@
 #!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
-#ASK THE USER A FEW QUESTIONS
-while true; do
-    read -p "Enter de database name: " dbname
-    case $dbname in
-        (*[a-z]*) break;;
-        q ) echo "Quitting the program"; exit;;
-        * ) echo "Please enter the database name ('q' to quit)";;
-    esac
+while getopts ":d:u:p:s:a:l:m:f:t:r:" opt; do
+  case ${opt} in
+    d )
+    dbname=$OPTARG
+    #echo $dbname
+      ;;
+    u )
+    username=$OPTARG
+    #echo $username
+      ;;
+    p )
+    password=$OPTARG
+    #echo $password
+      ;;
+    s )
+    datasource=$OPTARG
+    #echo $datasource
+      ;;
+    a )
+    study_area_layer=$OPTARG
+    #echo $study_area_layer
+      ;;
+    l )
+    land_use_layer=$OPTARG
+    #echo $land_use_layer
+      ;;
+    m )
+    distance=$OPTARG
+    #echo $distance
+      ;;
+    f )
+    formula=$OPTARG
+    #echo $formula
+      ;;
+    t )
+    type=$OPTARG
+    #echo $type
+      ;;
+    r )
+    resolution=$OPTARG
+    #echo $resolution
+      ;;
+    \? ) echo "Wrong parameter"
+	 echo "Usage: cmd [-d database name] [-u database username] [-p database password] [-s path to multilayer datasource] [-a study area layer name] [-l land user layer name] [-m analysis distance] [-f analysis formula] [-t] analysis type [-r raster output spatial resolution]"  && exit
+      ;;
+  esac
 done
 
-while true; do
-    read -p "Enter de database username: " username
-    case $username in
-        (*[a-z]*) break;;
-        q ) echo "Quitting the program"; exit;;
-        * ) echo "Please enter the database username ('q' to quit)";;
-    esac
-done
+##CHECK IF ALL PARAMETERS ARE SET
+if [ ! "$dbname" ] || [ ! "$username" ] || [ ! "$password" ] || [ ! "$datasource" ] || [ ! "$study_area_layer" ] || [ ! "$land_use_layer" ] || [ ! "$distance" ] || [ ! "$formula" ] || [ ! "type" ] || [ ! "$resolution" ]
+then
+    echo "Missing mandatory parameter"
+    echo "Usage: cmd [-d database name] [-u database username] [-p database password] [-s path to multilayer datasource] [-a study area layer name] [-l land user layer name] [-m analysis distance] [-f analysis formula] [-t] analysis type [-r raster output spatial resolution]"  && exit
+fi
 
-while true; do
-    read -p "Enter de database password: " password
-    case $password in
-        (*[a-z]*) break;;
-        q ) echo "Quitting the program"; exit;;
-        * ) echo "Please enter the database password ('q' to quit)";;
-    esac
-done
+#CHECK IF ANALYSIS DISTANCE AND RASTER RESOLUTION ARE INTEGERS
+if ! [[ "$distance" =~ ^[0-9]+$ ]] || ! [[ "$resolution" =~ ^[0-9]+$ ]]
+    then
+        echo "Distance and resolution parameters can only be integer values" && exit
+fi
 
-#CHECK IF IS POSSIBLE TO ESTABLISH A CONNECTION TO THE DATABASE AND IF IT HAS POSTGIS IN IT
+#CHECK IF FORMULA AND TYPE PARAMETERS ARE AS EXPECTED
+if ! [[ "$formula" == "li" ]] && ! [[ "$formula" == "ga" ]]
+    then
+        echo "Accepted values for the 'formula' paramters are 'li' or 'ga'" && exit
+fi
+
+if ! [[ "$type" == "bo" ]] && ! [[ "$type" == "bb" ]]
+    then
+        echo "Accepted values for the 'type' paramters are 'bo' or 'bb'" && exit
+fi
+
+##CHECK IF THE GPKG DATASOURCE EXIST
+if [ ! -f $datasource ]; then
+    echo "Input Datasource NOT found!" && exit
+else
+nopath=$(basename -- "$datasource")
+path=$(dirname "${datasource}")
+noext="${nopath%.*}"
+#echo $nopath
+#echo $path
+#echo $noext
+fi
+
+##CHECK IF THE PROVIDED DATASOURCE CONTAINS THE INPUT LAYERS AND IF THEY ARE THE PROPER TYPE AND HAVE THE PROPER CRS
+check_sa="$(ogrinfo -so $datasource $study_area_layer | grep $study_area_layer)"
+if [ "$check_sa" == "FAILURE: Couldn't fetch requested layer $study_area_layer!" ]
+then
+      echo "The datasurce does not contain a layer called '$study_area_layer'" && exit
+fi
+
+check_sa_geom="$(ogrinfo -so $datasource $study_area_layer | grep 'Geometry:')"
+#echo $check_sa_geom
+if ([ -z "$check_sa_geom" ]) || ([ "$check_sa_geom" != "Geometry: Polygon" ] && [ "$check_sa_geom" != "Geometry: Multi Polygon" ])
+then
+      echo "The '$study_area_layer' layer is not of type POLYGON or MULTIPOLYGON" && exit
+fi
+
+check_sa_proj="$(ogrinfo -so $datasource $study_area_layer | grep PROJCS)"
+if [ -z "$check_sa_proj" ]
+then
+      echo "The layer called '$study_area_layer' is NOT in a PROJECTED coordinate reference system" && exit
+fi
+
+check_lu="$(ogrinfo -so $datasource $land_use_layer | grep $land_use_layer)"
+if [ "$check_lu" == "FAILURE: Couldn't fetch requested layer $land_use_layer!" ]
+then
+      echo "The datasurce does not contain a layer called '$land_use_layer'" && exit
+fi
+
+check_lu_geom="$(ogrinfo -so $datasource $land_use_layer | grep 'Geometry:')"
+#echo $check_sa_geom
+if ([ -z "$check_lu_geom" ]) || ([ "$check_sa_geom" != "Geometry: Polygon" ] && [ "$check_sa_geom" != "Geometry: Multi Polygon" ])
+then
+      echo "The '$land_use_layer' layer is not of type POLYGON or MULTIPOLYGON" && exit
+fi
+
+check_lu_proj="$(ogrinfo -so $datasource $land_use_layer | grep PROJCS)"
+if [ -z "$check_lu_proj" ]
+then
+      echo "The layer called '$land_use_layer' is NOT in a PROJECTED coordinate reference system" && exit
+fi
+
+check_sa_proj_code="$(ogrinfo -so $datasource $study_area_layer | grep 'AUTHORITY' | tail -1)"
+check_lu_proj_code="$(ogrinfo -so $datasource $land_use_layer | grep 'AUTHORITY' | tail -1)"
+if [ "$check_sa_proj_code=" != "$check_lu_proj_code=" ]
+then
+      echo "The study area and land use CRSs do not match" && exit
+fi
+
+##CHECK IF IS POSSIBLE TO ESTABLISH A CONNECTION TO THE DATABASE AND IF IT HAS POSTGIS IN IT
 message=$(PGPASSWORD=$password psql -h localhost -d $dbname -U $username -c "SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';" 2>&1 >/dev/null)
 
 if [[ $message == *"FATAL"* ]]; then
@@ -41,100 +145,58 @@ if [[ $message != *"spatial_ref_sys"* ]]; then
   echo "The chosen PostgreSQL database does not have the PostGIS extension installed" && exit
 fi
 
-#ASK THE USER A FEW MORE QUESTIONS
-read -p "Absolute or relative path to a GEOPACKAGE datasource containing the study area boundaries, the land use map and the classification table: " datasource
-read -p "Analysis distance in map/CRS units (default=500): " distance
+##nopath=$(basename -- "$datasource")
+##path=$(dirname "${datasource}")
+##noext="${nopath%.*}"
 
-while true; do
-    read -p "Formula to use, 'gaussian' ('ga) or 'linear' ('li'): " formula
-    case $formula in
-        ga) break;;
-        li) break;;
-        q ) echo "Quitting the program"; exit;;
-        * ) echo "Please the formula to use ('q' to quit)";;
-    esac
-done
-#read -p "Formula to use, 'gaussian' ('ga) or 'linear' ('li') (default 'linear'): " formula
-
-while true; do
-    read -p "Compute distances with patches bounding boxes ('bb') or boundaries ('bo'): " type
-    case $type in
-        bb) break;;
-        bo) break;;
-        q ) echo "Quitting the program"; exit;;
-        * ) echo "Please choose bounding boxes or boundaries ('q' to quit)";;
-    esac
-done
-#read -p "Compute distances with patches bounding boxes ('bb') or boundaries ('bo') (default='bb')?: " type
-
-read -p "Raster output resolution in map/CRS units (must be an integer value, default=10): " resolution
-
-if [ -z "$distance" ]
-then
-      distance="500"
-fi
-
-if [ -z "$resolution" ]
-then
-      resolution="10"
-fi
-
-nopath=$(basename -- "$datasource")
-noext="${datasource%.*}"
-
-#SET THE NAME FOR A SCHEMA IN THE DATABASE THAT WILL HOLD THE INPUT DATA AND THE RESULTS
+##SET THE NAME FOR A SCHEMA IN THE DATABASE THAT WILL HOLD THE INPUT DATA AND THE RESULTS
 schemaname=$noext"_"$formula"_"$type"_"$distance"_"$(date +'%m_%d_%Y_%H_%M')
 
-#CHECK IF THE GPKG DATASOURCE EXIST
-if [ ! -f $datasource ]; then
-    echo "GPKG Datasource NOT found!" && exit
+##CREATE THE SCHEMA FOR THE RESULTS
+check_schema=$(PGPASSWORD=land6 psql -q -U land6 -d land6 -h localhost -c "SELECT schema_name FROM information_schema.schemata WHERE schema_name = '$schemaname';")
+if [[ $check_schema == *"1 row"* ]]; then
+  echo "Database schema name already exists, wait at least 1 minute before running the analysis" && exit
+else
+PGPASSWORD=$password psql -q -h localhost -d $dbname -U $username -c "CREATE SCHEMA $schemaname;"
 fi
 
-#epsg_sa="$(ogrinfo -so $sa_bound $noext | grep 3763)"
-
-#if [ -z "$epsg_sa" ]
-#then
-#      echo "Study Area Datasource is NOT in EPSG:3763" && exit
-#fi
-
-#CREATE THE SCHEMA FOR THE RESULTS
-PGPASSWORD=$password psql -q -U $username -d $dbname -h localhost -c "CREATE SCHEMA $schemaname;"
- 
 echo ""
 echo -e "Starting Process!"
+echo -e "Analysis Data and Time: ${RED}$(date +'%m/%d/%Y %H:%M')${NC}"
+echo -e "Analysis name: ${RED}$schemaname${NC}"
 echo ""
 
 #IMPORT THE STUDY AREA AND LAND USE LAYERS
-echo "Importing study area map"
-ogr2ogr -progress -f "PostgreSQL" "PG:host=localhost user=$username dbname=$dbname password=$password" -lco SCHEMA=$schemaname -lco GEOMETRY_NAME=geom -lco FID=gid -nln study_area -nlt MULTIPOLYGON $datasource study_area
-echo ""
-echo "Importing land use map"
-ogr2ogr -progress --config PG_USE_COPY YES -f "PostgreSQL" "PG:host=localhost user=$username dbname=$dbname password=$password" -lco SCHEMA=$schemaname -lco GEOMETRY_NAME=geom -lco FID=gid -nln land_use -nlt MULTIPOLYGON $datasource land_use
+echo "Importing study area map..."
+ogr2ogr -q -progress -f "PostgreSQL" "PG:host=localhost user=$username dbname=$dbname password=$password" -lco SCHEMA=$schemaname -lco GEOMETRY_NAME=geom -lco FID=gid -nln study_area -nlt MULTIPOLYGON $datasource study_area
+echo "Importing land use map..."
+ogr2ogr -q -progress --config PG_USE_COPY YES -f "PostgreSQL" "PG:host=localhost user=$username dbname=$dbname password=$password" -lco SCHEMA=$schemaname -lco GEOMETRY_NAME=geom -lco FID=gid -nln land_use -nlt MULTIPOLYGON $datasource land_use
 
-#CREATE THE SOURCE PARCELS LAYER
-PGPASSWORD=$password psql -h localhost -d $dbname -U $username -c "\
+echo -e "Processing the data within the database..."
+##CREATE THE SOURCE PARCELS LAYER
+PGPASSWORD=$password psql -q -h localhost -d $dbname -U $username -c "\
 CREATE TABLE $schemaname.source_parcels AS \
-SELECT a.*, ST_Envelope(a.geom) AS bbox \
+SELECT a.*, ST_MakeValid(a.geom) AS geom_valid, ST_Envelope(a.geom) AS bbox \
 FROM $schemaname.land_use a, $schemaname.study_area b \
 WHERE a.type = 'source' AND ST_Intersects(a.geom,b.geom) IS TRUE;"
 
-#CREATE THE TARGET PARCELS LAYER
-PGPASSWORD=$password psql -h localhost -d $dbname -U $username -c "\
+##CREATE THE TARGET PARCELS LAYER
+PGPASSWORD=$password psql -q -h localhost -d $dbname -U $username -c "\
 CREATE TABLE $schemaname.target_parcels AS \
-SELECT a.*, ST_Envelope(a.geom) AS bbox \
+SELECT a.*, ST_MakeValid(a.geom) AS geom_valid, ST_Envelope(a.geom) AS bbox \
 FROM $schemaname.land_use a, $schemaname.study_area b \
 WHERE a.type = 'target' AND ST_Intersects(a.geom,b.geom) IS TRUE;"
 
-#SET THE GEOMETRIES NAMES
+##SET THE GEOMETRIES NAMES
 if [ $type = "bb" ]
 then
 geom='bbox'
 elif [ $type = "bo" ]
 then
-geom='geom'
+geom='geom_valid'
 fi
 
-#SET THE FORUMLAS
+##SET THE FORUMLAS
 if [ $formula = "li" ]
 then
 query='CASE WHEN ST_Distance(target_parcels.'"$geom"', source_parcels.'"$geom"')=0 THEN source_parcels.value ELSE (1-(ST_Distance(target_parcels.'"$geom"',source_parcels.'"$geom"')/'"$distance"'))*source_parcels.value END AS value_target, '
@@ -143,8 +205,8 @@ then
 query='source_parcels.value*((2.718281828459045235360287471352662497757247093699959574966^(((ST_Distance(target_parcels.'"$geom"',source_parcels.'"$geom"')/'"$distance"')*(ST_Distance(target_parcels.'"$geom"',source_parcels.'"$geom"')/'"$distance"')*-4)+0.92))/sqrt(6.283185307179586476925286766559005768394338798750211641949)) AS value_target, '
 fi
 
-#RUN THE ANALYSIS   
-PGPASSWORD=$password psql -h localhost -d $dbname -U $username -c "\
+##RUN THE ANALYSIS
+PGPASSWORD=$password psql -q -h localhost -d $dbname -U $username -c "\
 CREATE TABLE $schemaname.raw_data AS \
 SELECT \
    row_number() OVER () AS gid, \
@@ -158,7 +220,7 @@ SELECT \
    source_parcels.geom AS geom_source, target_parcels.geom AS geom_target, \
    source_parcels.bbox AS bbox_source, target_parcels.bbox AS bbox_target \
  FROM \
-   (SELECT DISTINCT ON (geom) * 
+   (SELECT DISTINCT ON (geom) *
     FROM $schemaname.source_parcels) AS source_parcels \
  CROSS JOIN LATERAL \
    (SELECT * \
@@ -166,29 +228,32 @@ SELECT \
     ) AS target_parcels \
     WHERE ST_Distance(target_parcels.$geom, source_parcels.$geom) < $distance \
          ORDER BY distance;"
-         
-PGPASSWORD=$password psql -h localhost -d $dbname -U $username -c "\
+
+PGPASSWORD=$password psql -q -h localhost -d $dbname -U $username -c "\
         CREATE TABLE $schemaname.results AS \
-        SELECT 
+        SELECT
         row_number() OVER () AS gid, \
         id_target, class_target, \
         sum(value_target) AS value, \
-        ST_Union(geom_target) AS geom FROM 
+        ST_Union(geom_target) AS geom FROM
         $schemaname.raw_data \
         GROUP BY id_target,class_target,geom_target;"
 
-#RASTERIZE RESULTS        
-PGPASSWORD=$password psql -h localhost -d $dbname -U $username -c "\
+##RASTERIZE RESULTS
+PGPASSWORD=$password psql -q -h localhost -d $dbname -U $username -c "\
 CREATE TABLE $schemaname.result_raster AS
 select row_number() OVER () AS rid,
 ST_asRaster(geom, $resolution. , -$resolution. , '32BF', value, -9999)
 AS rast FROM $schemaname.results;"
 
-#EXPORT REUSULTS TO A GEOPACKAGE
-gdal_rasterize -l $schemaname.results -a value -tr $resolution $resolution -a_nodata -9999.0 -ot Float32 -of GTiff "PG:dbname=$dbname host=localhost port=5432 user=$username password=$password sslmode=disable" $schemaname'_results_rasterized'.tif
+##EXPORT REUSULTS TO A GEOPACKAGE
+echo -e "Exporting the results in Geopackage format..."
+gdal_rasterize -q -l $schemaname.results -a value -tr $resolution $resolution -a_nodata -9999.0 -ot Float32 -of GTiff "PG:dbname=$dbname host=localhost port=5432 user=$username password=$password sslmode=disable" $path/$schemaname'_results_rasterized'.tif
+gdal_translate -q -of GPKG $path/$schemaname'_results_rasterized'.tif $path/$schemaname.gpkg -co RASTER_TABLE=results_rasterized
+ogr2ogr -f GPKG $path/$schemaname.gpkg "PG:dbname=$dbname host=localhost port=5432 user=$username password=$password sslmode=disable schemas=$schemaname" -update
+rm $path/$schemaname'_results_rasterized'.tif
 
-gdal_translate -of GPKG $schemaname'_results_rasterized'.tif $schemaname.gpkg -co RASTER_TABLE=results_rasterized
-
-ogr2ogr -f GPKG $schemaname.gpkg "PG:dbname=$dbname host=localhost port=5432 user=$username password=$password sslmode=disable schemas=$schemaname" -update
-
-rm $schemaname'_results_rasterized'.tif
+echo ""
+echo -e "Analysis finished"
+output_abs_path="$(readlink -f $path/$schemaname.gpkg)"
+echo -e "Results saved in ${RED}$output_abs_path${NC}"
