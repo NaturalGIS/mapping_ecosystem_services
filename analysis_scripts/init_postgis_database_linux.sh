1ac08aa128de5086d06cd9cb3641c25dde1639ca
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2019-12-26 12:47:12 +0000

    Minor changes

--- analysis_scripts/init_postgis_database_linux.sh
+++ analysis_scripts/init_postgis_database_linux.sh
@@ -2,18 +2,18 @@
 while true; do
     read -p "Install PostgreSQL and PostGIS packages? " input_type
     case $input_type in
-        Y) break;;
-        N) break;;
+        y) break;;
+        n) break;;
         q ) echo "Quitting the program"; exit;;
         * ) echo "Please choose Y or N ('q' to quit)";;
     esac
 done
 
-if [ $input_type = 'Y' ]
+if [ $input_type = 'y' ]
 then
   sudo apt-get update
   sudo apt-get dist-upgrade
-  sudo apt-get install postgresql-10 postgresql-10-postgis-2.4 postgresql-10-postgis-scripts
+  sudo apt-get install postgresql-10 postgresql-10-postgis-2.4 postgresql-10-postgis-scripts gdal-bin
 fi
 
 read -p "PostgreSQL/PostGIS database name (default=land): "  dbname
@@ -29,6 +29,7 @@ then
       username="land"
 fi
 
+cd /tmp
 sudo -u postgres createuser $username -P
 sudo -u postgres createdb $dbname --o $username
 sudo -u postgres psql -d $dbname -c 'CREATE EXTENSION postgis;'
